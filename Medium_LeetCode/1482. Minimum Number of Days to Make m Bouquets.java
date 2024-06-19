d86471e9d0dc1efb6675a59f1623ed4d16f6660d
Justin Fei <justin.y.fei@gmail.com>
2024-06-18 23:51:12 -0400

    Update 1482. Minimum Number of Days to Make m Bouquets.java

--- Medium_LeetCode/1482. Minimum Number of Days to Make m Bouquets.java
+++ Medium_LeetCode/1482. Minimum Number of Days to Make m Bouquets.java
@@ -1,4 +1,53 @@
 class Solution {
+
+    // Return the number of maximum bouquets that can be made on day mid.
+    private int getNumOfBouquets(int[] bloomDay, int mid, int k) {
+        int numOfBouquets = 0;
+        int count = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            // If the flower is bloomed, add to the set. Else reset the count.
+            if (bloomDay[i] <= mid) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (count == k) {
+                numOfBouquets++;
+                count = 0;
+            }
+        }
+
+        return numOfBouquets;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        int start = 0;
+        int end = 0;
+        for (int day : bloomDay) {
+            end = Math.max(end, day);
+        }
+
+        int minDays = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (getNumOfBouquets(bloomDay, mid, k) >= m) {
+                minDays = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return minDays;
+    }
+}
+
+// -----------------------------------------------------------------------
+
+class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         if(((long) k * m) > bloomDay.length) return -1;
         int l = 1, r = 1000_000_000;
