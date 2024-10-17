9658a8e6aceb215b4695bbc3e74a8187668d62ac
Justin Fei <justin.y.fei@gmail.com>
2024-10-17 10:00:43 -0400

    Update 670. Maximum Swap.java

--- Medium_LeetCode/670. Maximum Swap.java
+++ Medium_LeetCode/670. Maximum Swap.java
@@ -2,18 +2,20 @@ class Solution {
     public int maximumSwap(int num) {
         char[] digits = Integer.toString(num).toCharArray();
 
-        int[] buckets = new int[10];
+        int[] lastPosition = new int[10];
 
         for (int i = 0; i < digits.length; i++) {
-            buckets[digits[i] - '0'] = i;
+            lastPosition[digits[i] - '0'] = i;
         }
 
         for (int i = 0; i < digits.length; i++) {
             for (int j = 9; j > digits[i] - '0'; j--) {
-                if (buckets[j] > i) {
+                 // Compare the digit index, if the index is bigger,
+                 // it means it's behind. We should swap it.
+                if (lastPosition[j] > i) {
                     char tmp = digits[i];
-                    digits[i] = digits[buckets[j]];
-                    digits[buckets[j]] = tmp;
+                    digits[i] = digits[lastPosition[j]];
+                    digits[lastPosition[j]] = tmp;
                     return Integer.valueOf(new String(digits));
                 }
             }
