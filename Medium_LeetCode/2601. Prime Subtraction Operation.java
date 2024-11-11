606824293f76b8308eb5b4d04bb2a940ba0eeca3
Justin Fei <justin.y.fei@gmail.com>
2024-11-11 15:01:40 -0500

    Update 2601. Prime Subtraction Operation.java

--- Medium_LeetCode/2601. Prime Subtraction Operation.java
+++ Medium_LeetCode/2601. Prime Subtraction Operation.java
@@ -1,32 +1,25 @@
 class Solution {
     private boolean isPrime(int x) {
         if (x == 1) return false;
-        for (int i = 2; i * i <= x; i++) {
-            if (x % i == 0) {
+
+        for (int i = 2; i * i <= x; i++)
+            if (x % i == 0)
                 return false;
-            }
-        }
+
         return true;
     }
 
     public boolean primeSubOperation(int[] nums) {
-        int prev = 0;
-
+        if (nums == null || nums.length == 0) return false;
+        if (nums.length == 1) return true;
+        int candidate, prev = 0;
         for (int num : nums) {
-            if (num <= prev) {
-                return false;
-            }
+            if (num <= prev) return false;
 
-            int prime = num - prev - 1;
-            while (prime > 0 && !isPrime(prime)) {
-                prime--;
-            }
+            candidate = num - prev - 1;
+            while (candidate > 0 && !isPrime(candidate)) candidate--;
 
-            if (prime == 0) {
-                prev = num;
-            } else {
-                prev = num - prime;
-            }
+            prev = num - candidate;
         }
 
         return true;
