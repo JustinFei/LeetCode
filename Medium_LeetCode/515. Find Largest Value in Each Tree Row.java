106a16f67cc64df9892293272e6b177d64eb17a3
Justin Fei <justin.y.fei@gmail.com>
2024-12-25 10:37:53 -0500

    Update 515. Find Largest Value in Each Tree Row.java

--- Medium_LeetCode/515. Find Largest Value in Each Tree Row.java
+++ Medium_LeetCode/515. Find Largest Value in Each Tree Row.java
@@ -4,26 +4,61 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-public class Solution {
+
+ // -------------- BFS -----------------
+class Solution {
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            int max = Integer.MIN_VALUE;
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = queue.poll();
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        TreeNode cur;
+        int max, size;
+        while (!q.isEmpty()) {
+            max = Integer.MIN_VALUE;
+            size = q.size();
+            while (size -- > 0) {
+                cur = q.poll();
                 max = Math.max(cur.val, max);
-                if (cur.left != null) queue.offer(cur.left);
-                if (cur.right != null) queue.offer(cur.right);
+                if (cur.left != null) q.offer(cur.left);
+                if (cur.right != null) q.offer(cur.right);
             }
             res.add(max);
         }
         return res;
+    }
+}
+
+// -------------- DFS -----------------
+
+
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res, 0);
+        return res;
+    }
+
+    public void dfs(TreeNode root, List<Integer> res, int level) {
+        if (root == null) return;
+
+        if (level == res.size()) {
+            res.add(root.val);
+        } else {
+            res.set(level, Math.max(res.get(level), root.val));
+        }
+
+        dfs(root.left, res, level + 1);
+        dfs(root.right, res, level + 1);
     }
 }
