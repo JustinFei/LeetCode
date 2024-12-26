7fa573d84ba4a67805a4005cc6aaa4b712eebbac
Justin Fei <justin.y.fei@gmail.com>
2024-12-26 11:17:44 -0500

    Update 494. Target Sum.java

--- Medium_LeetCode/494. Target Sum.java
+++ Medium_LeetCode/494. Target Sum.java
@@ -1,18 +1,57 @@
-public class Solution {
-    public int findTargetSumWays(int[] nums, int S) {
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
         if (nums == null || nums.length == 0) return 0;
-        int[] res = { 0 };
-        
-        helper(0, 0, res, nums, S);
+        int[] res = new int[1];
+        dfs(nums, 0, 0, target, res);
         return res[0];
     }
-    
-    private void helper(int index, int tmp, int[] res, int[] nums, int target) {
-        if (index == nums.length) {
-            if (tmp == target) res[0]++;
+
+    private void dfs(int[] nums, int i, int c, int t, int[] res) {
+        if (i == nums.length) {
+            if (c == t) res[0]++; 
             return;
         }
-        helper(index + 1, tmp + nums[index], res, nums, target);
-        helper(index + 1, tmp - nums[index], res, nums, target);
+        dfs(nums, i + 1, c + nums[i], t, res);
+        dfs(nums, i + 1, c - nums[i], t, res);
+    }
+}
+
+// -------------------------------
+
+public class Solution {
+
+    // Sum of all elements in the array
+    int totalSum;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        totalSum = Arrays.stream(nums).sum();
+
+        int[][] memo = new int[nums.length][2 * totalSum + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return calculateWays(nums, 0, 0, target, memo);
+    }
+
+    private int calculateWays(int[] nums, int i, int c, int t, int[][] memo) {
+        if (i == nums.length) {
+            // Check if the current sum matches the target
+            if (c == t) return 1;
+            else return 0;
+        } 
+
+        // Check if the result is already computed
+        if (memo[i][c + totalSum] != Integer.MIN_VALUE) return memo[i][c + totalSum];
+        
+        // Calculate ways by adding the current number
+        int add = calculateWays(nums, i + 1, c + nums[i], t, memo);
+
+        // Calculate ways by subtracting the current number
+        int subtract = calculateWays(nums, i + 1, c - nums[i], t, memo);
+
+        // Store the result in memoization table
+        memo[i][c + totalSum] = add + subtract;
+
+        return memo[i][c + totalSum];
     }
 }
