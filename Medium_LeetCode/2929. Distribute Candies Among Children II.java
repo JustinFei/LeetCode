571bb2a17df7ae9d35c7d5466edd3d71422bb75a
Justin Fei <justin.y.fei@gmail.com>
2025-06-01 10:39:13 -0400

    Update 2929. Distribute Candies Among Children II.java

--- Medium_LeetCode/2929. Distribute Candies Among Children II.java
+++ Medium_LeetCode/2929. Distribute Candies Among Children II.java
@@ -16,3 +16,37 @@ class Solution {
         return res;
     }
 }
+
+// -----------------------------------
+
+class Solution {
+
+    public long distributeCandies(int n, int limit) {
+        long ans = 0;
+        for (int i = Math.max(0, n - limit * 2); i <= Math.min(limit, n); i++) {
+            ans += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
+        }
+        return ans;
+    }
+}
+
+// ---------------------------------
+
+class Solution {
+
+    public long distributeCandies(int n, int limit) {
+        return (
+            cal(n + 2) -
+            3 * cal(n - limit + 1) +
+            3 * cal(n - (limit + 1) * 2 + 2) -
+            cal(n - 3 * (limit + 1) + 2)
+        );
+    }
+
+    public long cal(int x) {
+        if (x < 0) {
+            return 0;
+        }
+        return ((long) x * (x - 1)) / 2;
+    }
+}
